d97150edf23f6998247a97846c95d472b438cff3
Aesha <paesha1099@gmail.com>
2025-08-20 14:15:39 +0000

    Day-4 array_mastery

---
+++ Day-4/array_mastery.php
@@ -0,0 +1,18 @@
+<?php
+// $skuList =['SKU-01','SKU-02', 'SKU-03'];
+// $firstSku = $skuList['0'];
+// $skuList['1'] = 'SKU-002-UPDATED';
+
+// echo "<pre>"; print_r($skuList['0']);
+
+// Associative Arrays
+
+// $product = [
+//     'sku' => 'M-TSHIRT-XL',
+//     'name' => 'Magento Tee XL',
+//     'price' => 999.00
+// ];
+
+// $sku = $product['sku'];
+// $product['price'] = 1099.00;
+
